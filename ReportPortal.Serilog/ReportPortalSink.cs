718b09a003cb18ec4b22257bed09cde420be5de2
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2018-10-12 14:24:09 +0300

    First implementation

---
+++ ReportPortal.Serilog/ReportPortalSink.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using ReportPortal.Client.Models;
+using ReportPortal.Shared;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace ReportPortal.Serilog
+{
+    /// <summary>
+    /// Sink for reporting logs directly to Report Portal.
+    /// Logs will be viewable under current test item from shared context.
+    /// </summary>
+    public class ReportPortalSink : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+
+        public ReportPortalSink(IFormatProvider formatProvider)
+        {
+            _formatProvider = formatProvider;
+        }
+        protected Dictionary<LogEventLevel, LogLevel> LevelMap = new Dictionary<LogEventLevel, LogLevel>();
+
+        public ReportPortalSink()
+        {
+            LevelMap[LogEventLevel.Debug] = LogLevel.Debug;
+            LevelMap[LogEventLevel.Error] = LogLevel.Error;
+            LevelMap[LogEventLevel.Fatal] = LogLevel.Error;
+            LevelMap[LogEventLevel.Information] = LogLevel.Info;
+            LevelMap[LogEventLevel.Verbose] = LogLevel.Trace;
+            LevelMap[LogEventLevel.Warning] = LogLevel.Warning;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var message = logEvent.RenderMessage(_formatProvider);
+
+            var level = LogLevel.Info;
+            if (LevelMap.ContainsKey(logEvent.Level))
+            {
+                level = LevelMap[logEvent.Level];
+            }
+
+            Bridge.LogMessage(level, message);
+        }
+    }
+}
---
+++ ReportPortal.Serilog/ReportPortalSinkExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+using Serilog.Configuration;
+using System;
+
+namespace ReportPortal.Serilog
+{
+    public static class ReportPortalSinkExtensions
+    {
+        public static LoggerConfiguration ReportPortal(this LoggerSinkConfiguration loggerConfiguration, IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new ReportPortalSink(formatProvider));
+        }
+    }
+}
